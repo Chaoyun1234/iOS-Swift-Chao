bd3ca768f36429b174c1cb7d7bca772019f03b6e
bys <bys@bysdembp-6.guest.corp.microsoft.com>
2017-07-03 19:36:33 +0800

    Initial Commit

---
+++ iOS-Swift-Chao/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iOS-Swift-Chao
+//
+//  Created by bys on 03/07/2017.
+//  Copyright © 2017 bys. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
